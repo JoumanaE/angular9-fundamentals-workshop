c7f39ffa70d5633f8b439f87bd42a15b61263e3c
joumanae <joumana.codes@gmail.com>
2020-06-10 07:47:18 -0500

    first commit

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,9 +3,11 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
+  title = 'Hello Workshop';
+  currentLesson = null;
 
   courseLessons = [
     { title: 'Hello Angular' },
@@ -18,9 +20,12 @@ export class HomeComponent implements OnInit {
     { title: 'Unit Testing Fundamentals' },
   ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  selectLesson(lesson) {
+    console.log('SELECT LESSON FIRED!', lesson);
+    this.currentLesson = lesson;
   }
-
 }
