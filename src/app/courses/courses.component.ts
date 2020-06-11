92bd30a45088768c07ff388f2a78018ed9c04074
joumanae <joumana.codes@gmail.com>
2020-06-11 11:01:59 -0500

    ends the services section

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -4,7 +4,7 @@ import { CoursesService } from '../shared/services/courses.service';
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
-  styleUrls: ['./courses.component.scss']
+  styleUrls: ['./courses.component.scss'],
 })
 export class CoursesComponent implements OnInit {
   // CHALLENGE
@@ -15,11 +15,11 @@ export class CoursesComponent implements OnInit {
 
   courses = null;
 
-  constructor(private coursesService: CoursesService) { }
+  constructor(private coursesService: CoursesService) {}
 
   ngOnInit(): void {
     this.resetSelectedCourse();
-    this.courses = this.coursesService.all();
+    this.loadCourses();
   }
 
   resetSelectedCourse() {
@@ -28,7 +28,7 @@ export class CoursesComponent implements OnInit {
       title: '',
       description: '',
       percentComplete: 0,
-      favorite: false
+      favorite: false,
     };
 
     this.selectedCourse = emptyCourse;
@@ -38,16 +38,31 @@ export class CoursesComponent implements OnInit {
     this.selectedCourse = course;
   }
 
+  loadCourses() {
+    this.coursesService.all().subscribe((courses) => (this.courses = courses));
+  }
+
+  refreshCourses() {
+    this.resetSelectedCourse();
+    this.loadCourses();
+  }
+
   saveCourse(course) {
-    if(course.id) {
-      this.coursesService.update(course);
+    if (course.id) {
+      this.coursesService
+        .update(course)
+        .subscribe((result) => this.refreshCourses());
     } else {
-      this.coursesService.create(course);
+      this.coursesService
+        .create(course)
+        .subscribe((result) => this.refreshCourses());
     }
   }
 
   deleteCourse(courseId) {
-    this.coursesService.delete(courseId);
+    this.coursesService
+      .delete(courseId)
+      .subscribe((result) => this.refreshCourses());
   }
 
   cancel() {
--- src/app/shared/services/courses.service.ts
+++ src/app/shared/services/courses.service.ts
@@ -1,45 +1,40 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+const BASE_URL = 'http://localhost:3000/';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CoursesService {
-  private courses = [
-    {
-      id: 1,
-      title: 'Angular 9 Fundamentals!!',
-      description: 'Learn the fundamentals of Angular 9',
-      percentComplete: 26,
-      favorite: true
-    },
-    {
-      id: 2,
-      title: 'JavaScript The Really REALLY HARD PARTS!!',
-      description: 'Worship Will Sentance',
-      percentComplete: 50,
-      favorite: true
-    }
-  ];
+  private model = 'courses';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
 
   all() {
-    return this.courses;
+    return this.http.get(this.getUrl());
   }
 
   find(courseId) {
-
+    return this.http.get(this.getUrlById(courseId));
   }
 
   create(course) {
-    console.log('CREATE COURSE', course);
+    return this.http.post(this.getUrl(), course);
   }
 
   update(course) {
-    console.log('UDDATE COURSE', course);
+    return this.http.post(this.getUrlById(course.id), course);
   }
 
   delete(courseId) {
-    console.log('DLETE COURSE', courseId);
+    return this.http.delete(this.getUrlById(courseId));
+  }
+  private getUrl() {
+    return `${BASE_URL}${this.model}`;
+  }
+
+  private getUrlById(id) {
+    return `${this.getUrl()}/${id}`;
   }
 }
