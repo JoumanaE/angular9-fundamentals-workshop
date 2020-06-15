bf6104389b0eef52a39095577bafbf6d6c31d918
joumanae <joumana.codes@gmail.com>
2020-06-15 08:19:44 -0500

    Last part of component testing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { CoursesComponent } from './courses/courses.component';
 import { HomeComponent } from './home/home.component';
+import { UsersComponent } from './users/users.component';
 
 // CHALLENGE
 // STEP 1: Generate a new users component
@@ -12,15 +13,17 @@ import { HomeComponent } from './home/home.component';
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'courses', component: CoursesComponent },
+  { path: 'users', component: UsersComponent },
   {
     path: 'login',
-    loadChildren: () => import('./login/login.module').then(m => m.LoginModule)
+    loadChildren: () =>
+      import('./login/login.module').then((m) => m.LoginModule),
   },
-  { path: '**', redirectTo: '/home' }
+  { path: '**', redirectTo: '/home' },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/courses/courses.component.spec.ts
+++ src/app/courses/courses.component.spec.ts
@@ -1,25 +1,45 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CoursesComponent } from './courses.component';
+import { CoursesService } from '../shared/services/courses.service';
+import { DebugElement } from '@angular/core';
+
+const coursesServicesStub = {
+  all: () => {
+    return {
+      subscribe: () => {},
+    };
+  },
+};
 
 describe('CoursesComponent', () => {
   let component: CoursesComponent;
   let fixture: ComponentFixture<CoursesComponent>;
+  let de: DebugElement;
+  let coursesService: CoursesService;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ CoursesComponent ]
-    })
-    .compileComponents();
+      declarations: [CoursesComponent],
+      providers: [{ provide: CoursesService, useValue: coursesServicesStub }],
+    }).compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(CoursesComponent);
     component = fixture.componentInstance;
+    de = fixture.debugElement;
+    coursesService = de.injector.get(CoursesService);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should call coursesService.delete on deleteCourse', () => {
+    spyOn(coursesService, 'delete').and.callThrough();
+    component.deleteCourse(1);
+    expect(coursesService.delete).toHaveBeenCalledWith(1);
   });
 });
